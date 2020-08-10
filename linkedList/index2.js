eaee518bca2f14f7651a08741fe19b1bc5fd60db
kongjun0320 <1741142595@qq.com>
2020-08-10 16:46:50 +0800

    链表2

---
+++ linkedList/index2.js
@@ -0,0 +1,27 @@
+const a = { value: 'a' }
+const b = { value: 'b' }
+const c = { value: 'c' }
+const d = { value: 'd' }
+
+a.next = b
+b.next = c
+c.next = d
+
+// 遍历链表
+
+// let p = a
+// while(p){
+//   console.log(p.value)
+//   p = p.next
+// }
+
+// 插入
+
+const e = { value: 'e' }
+c.next = e
+e.next = d
+
+// 删除
+c.next = d
+
+console.log(a)
